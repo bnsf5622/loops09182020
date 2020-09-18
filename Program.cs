d768ed53e939ac309a0f019a3752bcec754acecd
chuck francis <chuck_francis10@yahoo.com>
2020-09-18 16:13:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace loops09182020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's Spring...!");
+            for (int i = 0; i < 10; i++)
+             Console.WriteLine(i + ", ");    
+                {
+                    Console.WriteLine(" Oh no, it's Fall...");
+                    for ( int i = 10; i > 0; i--)
+                    {
+                        Console.WriteLine(i + ","); 
+                    }
+                }
+            }
+        }
+    }
